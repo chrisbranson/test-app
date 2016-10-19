5274352578dd2cdab1b4975628182ca46724cf75
Chris Branson <chris.branson@current-rms.com>
2016-10-19 15:23:51 +0100

    Added micro post relationships, passwords and email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
+	has_many :microposts, :dependent => :delete_all
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
 	validates :name, presence: true
-	validates :email, presence: true, uniqueness: true
+	validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+	validates :password, length: { minimum: 6 }
+
+	has_secure_password
 end
