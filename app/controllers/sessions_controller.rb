aa780013cc96588fb168b68cec542eab705bb469
Chris Branson <chris.branson@current-rms.com>
2016-10-20 11:27:05 +0100

    Add sign in and sign out functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+
+	# when user asks for the signin page
+	def new
+	end
+
+	# user clicks the sign in button
+	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+
+	  if user.present? && user.authenticate(params[:session][:password])
+	    # Sign the user in and redirect to the user's show page.
+	    sign_in user
+      redirect_to user
+	  else
+	    # Create an error message and re-render the signin form.
+	    flash.now[:error] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+	  end
+	end
+
+	# user clicks sign out
+	def destroy
+		sign_out
+    redirect_to root_url
+	end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+	before_create :create_remember_token
+	before_save { self.email = email.downcase }
+
 	has_many :microposts, :dependent => :delete_all
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,4 +11,18 @@ class User < ActiveRecord::Base
 	validates :password, length: { minimum: 6 }
 
 	has_secure_password
+
+	def self.new_remember_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def self.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+  def create_remember_token
+  	self.remember_token = User.digest(User.new_remember_token)
+  end
 end
